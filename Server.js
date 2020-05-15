7c72581ac2b3b7a9a7ab3397551154365b5095f3
wH1t3-l0tUs <trung.bqt97@gmail.com>
2020-05-15 17:33:42 +0700

    commit

--- Server.js
+++ Server.js
@@ -1,17 +1,17 @@
-var express = require('express');
+const PORT = process.env.PORT || 3000;
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app).listen(PORT);
+var io = require("socket.io")(server);
 
-server.listen(3000);
-io.on('connection', function (socket) {
-  console.log('Someone connected' + socket.id);
-  socket.on('user-joined', (data) => {
-    console.log(data + ' joined');
-    io.sockets.emit('server-noti-user-joined', data);
+io.on("connection", function (socket) {
+  console.log("Someone connected" + socket.id);
+  socket.on("user-joined", (data) => {
+    console.log(data + " joined");
+    io.sockets.emit("server-noti-user-joined", data);
   });
-  socket.on('user-send-message', (data) => {
-    console.log('da nhan tin nhan ', data);
-    io.sockets.emit('server-send-message', data);
+  socket.on("user-send-message", (data) => {
+    console.log("da nhan tin nhan ", data);
+    io.sockets.emit("server-send-message", data);
   });
 });
